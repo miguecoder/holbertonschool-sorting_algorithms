a794f362b17e23a160f516324406d81666892a75
miguecoder <mmarqzm@gmail.com>
2022-06-21 17:29:47 -0500

    function that sorts an array of integers in ascending order using the Bubble sort algorithm

--- sort.h
+++ sort.h
@@ -9,12 +9,13 @@
  *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
